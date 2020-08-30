f4fed0d7f4f06348de7942c339bb9d9213e4feee
Mateus Bezerra <mateusbzerra@gmail.com>
2020-08-29 21:01:20 -0300

    tests: refactor when add a new reminder

--- src/components/ReminderForm/test.spec.tsx
+++ src/components/ReminderForm/test.spec.tsx
@@ -6,7 +6,12 @@ import App from '../../App';
 
 describe('ReminderForm', () => {
   it('should be able to add a new reminder', async () => {
-    const { getByText, getByTestId, getByPlaceholderText } = render(<App />);
+    const {
+      getAllByText,
+      getByText,
+      getByTestId,
+      getByPlaceholderText,
+    } = render(<App />);
     act(() => {
       fireEvent.click(getByText('Add Reminder'));
     });
@@ -19,7 +24,7 @@ describe('ReminderForm', () => {
 
     await act(async () => {
       fireEvent.change(titleInput, {
-        target: { value: 'Add new Reminder' },
+        target: { value: 'Title of a new Reminder' },
       });
       fireEvent.change(dateInput, { target: { value: '2020-8-6' } });
       fireEvent.change(hourInput, { target: { value: '12' } });
@@ -32,6 +37,6 @@ describe('ReminderForm', () => {
       fireEvent.click(getByText('Save'));
     });
 
-    expect(getByText('Reminder successfully created')).toBeTruthy();
+    expect(getAllByText('Title of a new Reminder')).toBeTruthy();
   });
 });
