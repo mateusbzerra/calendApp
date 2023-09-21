298ea7fa11354c666df4168a0764c1e4e5da6913
Mateus Bezerra <mateusbzerra@gmail.com>
2023-09-21 00:33:30 -0300

    refactor: separate calendar constants

---
+++ src/contants.ts
@@ -0,0 +1,24 @@
+export const SHORT_WEEK_NAMES = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+];
+
+export const MONTH_NAMES = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
